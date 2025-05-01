25a985a0478d042ffc44ccd7a37204ed1aebb017
jinlovespho <jhw9811@gmail.com>
2025-05-01 15:01:43 +0000

    20250502

--- pho_test_metric.py
+++ pho_test_metric.py
@@ -20,43 +20,21 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 metric_psnr = pyiqa.create_metric('psnr', device=device)
 metric_ssim = pyiqa.create_metric('ssimc', device=device)
 metric_lpips = pyiqa.create_metric('lpips', device=device)
+metric_dists = pyiqa.create_metric('dists', device=device)
 # metric_fid = pyiqa.create_metric('fid', device=device)
-# metric_musiq = pyiqa.create_metric('musiq', device=device)
-# metric_maniqa = pyiqa.create_metric('maniqa', device=device)
+metric_niqe = pyiqa.create_metric('niqe', device=device)
+metric_musiq = pyiqa.create_metric('musiq', device=device)
+metric_maniqa = pyiqa.create_metric('maniqa', device=device)
+metric_clipiqa = pyiqa.create_metric('clipiqa', device=device)
 
 psnr = metric_psnr(img0, img1)
 ssim = metric_ssim(img0, img1)
 lpip = metric_lpips(img0, img1)
+dists = metric_dists(img0, img1)
 # fid = metric_fid(img0, img1)
-
-# musiq = metric_musiq(img0, img1)
-# maniqa = metric_maniqa(img0, img1)
-
-# psnr metric 
-from diffbir.utils.common import calculate_psnr_pt 
-psnr2 = calculate_psnr_pt(img0, img1, crop_border=0)
-
-# # ssim metric 
-# from diffbir.utils.common import calculate_ssim_pt
-# ssim2 = calculate_ssim_pt(img0, img1, crop_border=0)
-
-# # lpips metric 
-loss_fn_alex = lpips.LPIPS(net='alex') # best forward scores
-lpip2 = loss_fn_alex(img0, img1, normalize=True)
-
-
-from IQA_pytorch import SSIM, GMSD, LPIPSvgg, DISTS
-D = SSIM(channels=3)
-# Calculate score of the image X with the reference Y
-# X: (N,3,H,W) 
-# Y: (N,3,H,W) 
-# Tensor, data range: 0~1
-ssim3  = D(img0, img1, as_loss=False) 
-
-# from basicsr.metrics import calculate_psnr_pt, calculate_ssim
-
-# psnr3=basicsr.metrics.calculate_psnr_pt(img0, img1, crop_border=0)
-# ssim3=basicsr.metrics.calculate_ssim_pt(img0, img1, crop_border=0)
-
+niqe = metric_niqe(img0, img1)
+musiq = metric_musiq(img0, img1)
+maniqa = metric_maniqa(img0, img1)
+clipiqa = metric_clipiqa(img0, img1)
 
 breakpoint()
