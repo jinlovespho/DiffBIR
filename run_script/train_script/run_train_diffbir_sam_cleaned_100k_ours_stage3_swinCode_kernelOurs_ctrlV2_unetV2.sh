544a8169f5d5ce82e0c0a171c9828ff7a7924c37
jinlovespho <msjchr922@gmail.com>
2025-05-09 11:46:18 +0000

    20250509

---
+++ run_script/train_script/run_train_diffbir_sam_cleaned_100k_ours_stage3_swinCode_kernelOurs_ctrlV2_unetV2.sh
@@ -0,0 +1,6 @@
+
+# try1: finetune testr or testr_ctrlnet, using null prompt
+
+CUDA_VISIBLE_DEVICES=0,1 accelerate launch --main_process_port 29501 train.py      --config configs/train/train_diffbir_sam_cleaned_100k_ours_stage3_swinCode_kernelOurs_ctrlV2_unetV2.yaml \
+                                                                                --config_testr testr/configs/TESTR/Pretrain/TESTR_R_50_Polygon.yaml \
+
